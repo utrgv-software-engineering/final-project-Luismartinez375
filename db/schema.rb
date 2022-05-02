ff0ffe8ec49701b0cbc4462a79e6d91a85b2a2f1
Abel Ochoa Garcia <abel.ochoa01@utrgv.edu>
2022-05-02 16:21:56 +0000

    fixed problems with account_id not in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_170822) do
+ActiveRecord::Schema.define(version: 2022_05_02_075040) do
 
   create_table "grades", force: :cascade do |t|
     t.string "student_id"
@@ -23,13 +23,12 @@ ActiveRecord::Schema.define(version: 2020_04_28_170822) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    #adding account_id
-    t.integer "account_id", default: 1, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "account_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
