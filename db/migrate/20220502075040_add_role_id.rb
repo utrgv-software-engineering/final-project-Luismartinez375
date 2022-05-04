622b52d41a1263b9738b375077a10136c3b5e1df
Abel Ochoa Garcia <abel.ochoa01@utrgv.edu>
2022-05-04 02:53:46 +0000

    fixed default account_id value

--- db/migrate/20220502075040_add_role_id.rb
+++ db/migrate/20220502075040_add_role_id.rb
@@ -1,5 +1,5 @@
 class AddRoleId < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :account_id, :integer, default: 0, null: false
+    add_column :users, :account_id, :integer, default: 1, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_05_02_075040) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "account_id", default: 0, null: false
+    t.integer "account_id", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
